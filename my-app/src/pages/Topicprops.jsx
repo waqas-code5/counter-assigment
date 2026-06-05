505d956b06332f8f4f11c2ece58a75dd74412b47
WaqasKhan <jw5447857@gmail.com>
2026-06-05 10:37:12 +0500

    first commit

--- my-app/src/pages/Topicprops.jsx
+++ my-app/src/pages/Topicprops.jsx
@@ -1,4 +1,5 @@
-import{ useState } from "react";
+import { useState } from "react";
+
 function Topicprops(){
     const[val , setval] = useState("")
     const getval = (e)=>{
@@ -15,11 +16,10 @@ function Topicprops(){
       setText("");
     }
 
-
     return(
         <>
-        <h1>{text}</h1>
-        <input type="text" placeholder="enter value" value={val} onChange={getval} />
+        <h1>{val}</h1>
+        <input type="text" placeholder="enter value" onChange={getval} />
         <button onClick={showtext}>click me</button>
         <button onClick={resetall}>reset me</button>
         </>
